9ea617d27dc6550bd780925fe1469dbbbe66c692
wi11-holdsworth <qyp4l7a5@duck.com>
2024-07-08 12:22:59 +0000

    init

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+        nixos-wsl = {
+            url = "github:nix-community/NixOS-WSL/main";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = { self, nixpkgs, nixos-wsl, ... }@inputs: 
+    let
+        system = "x86_64-linux";
+        pkgs = import nixpkgs {
+            inherit system;
+            config = {
+                allowUnfree = true;
+            };
+        };
+    in 
+    {
+        nixosConfigurations = {
+            nixos = nixpkgs.lib.nixosSystem {
+                system = "x86_64-linux"; # weird bug requires this to be here
+                specialArgs = { inherit inputs; };
+                modules = [
+                    ./configuration.nix
+                    nixos-wsl.nixosModules.default
+                    inputs.home-manager.nixosModules.default
+                ];
+            };
+        };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,62 @@
+{ config, pkgs, ... }:
+
+{
+    home = {
+        username = "will";
+        homeDirectory = "/home/will";
+        stateVersion = "24.05";
+        packages = with pkgs; [
+            bash
+            git
+            starship
+        ];
+        sessionVariables = {
+            EDITOR = "vim";
+            FLAKE = "/home/will/.dotfiles";
+        };
+    };
+
+    programs = {
+        home-manager.enable = true;
+
+        bash = {
+            enable = true;
+            shellAliases = {
+                nos = "nh os switch ~/.dotfiles"; 
+                ls = "eza --group-directories-first --icons"; 
+                la = "ls -a";
+                ll = "la -l";
+                lt = "la -T";
+                ga = "git add .";
+                gc = "git commit";
+                gp = "git push";
+                gacp = "ga && gc && gp";
+                dots = "cd ~/.dotfiles && clear && ls -T && echo";
+                editcf = "vi ~/.dotfiles/hosts/$HOSTNAME/configuration.nix";
+                edithm = "vi ~/.dotfiles/hosts/$HOSTNAME/home.nix";
+                getflakey = ''nix flake new -t github:nix-community/nix-direnv . && echo "use flake" >> .envrc && direnv allow'';
+            };
+        };
+        
+        git = {
+            enable = true;
+            userName = "wi11-holdsworth";
+            userEmail = "qyp4l7a5@duck.com";
+            extraConfig.init.defaultBranch = "main";
+            delta.enable = true;
+        };
+        
+        starship = {
+            enable = true;
+            settings = {
+            character = {
+                success_symbol = "[➜](bold green) ";
+                error_symbol = "[➜](bold red) ";
+            };
+            add_newline = false;
+            cmd_duration.disabled = true;
+            line_break.disabled = true;
+            };
+        };
+    };
+}
