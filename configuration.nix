9180a2890f383c60fc256117474329493d8ff7f1
wi11-holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2024-07-09 02:11:06 +0000

    move pkgs around and add some nvim config

--- configuration.nix
+++ configuration.nix
@@ -3,27 +3,37 @@
 {
   # package management
   environment.systemPackages = with pkgs; [
+    github-cli
+    home-manager
+    nh
+
+    # rust rewrites
     bat
     delta
     dust
     eza
-    github-cli
+    fd
     gping
-    home-manager
-    nh
-    nodejs
-    wget
+    ripgrep-all
     zellij
   ];
 
-  # vscode server
   programs = {
+    # vscode server
     nix-ld = {
       enable = true;
       package = pkgs.nix-ld-rs;
     };  
     nixvim = {
       enable = true;
+      options = {
+        shiftwidth = 2;
+      	number = true;
+	relativenumber = true;
+      };
+      colorschemes.ayu.enable = true;
+      extraConfigVim = ''
+      '';
     };
   };
 
